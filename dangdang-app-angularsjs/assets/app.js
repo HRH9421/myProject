eb3031e227961c8953436673d20de59978e5f916
HRH9421 <hrh9421@foxmail.com>
2016-10-19 17:03:27 +0800

    没啥东西

--- dangdang-app-angularsjs/assets/app.js
+++ dangdang-app-angularsjs/assets/app.js
@@ -1,1 +1,30 @@
+
 var app = angular.module('app',['ngRoute'])
+app.config(['$routeProvider'],function($routeProvider){
+  $routeProvider
+    .when('/',{
+      templateUrl:'./tpl/index.html',
+      controller:''
+    })
+    .when('/blog',{
+      templateUrl:'./tpl/index.html',
+      controller:''
+    })
+    .when('/book',{
+      templateUrl:'./tpl/book.html',
+      controller:'bookController'
+    })
+    .when('/about_me',{
+      templateUrl:'./tpl/about_me.html',
+      controller:''
+    })
+    .otherwise({
+      redirectTo:'/'
+    })
+})
+app.controller('footerCtrl',['$scope',function($scope){
+  $scope.selectedIndex = 0;
+  $scope.linkTo = function(index){
+    $scope.selectedIndex = index;
+  }
+}])
--- dangdang-app-angularsjs/assets/controllers/main_controller.js
+++ dangdang-app-angularsjs/assets/controllers/main_controller.js
@@ -1,5 +1,9 @@
-app.controller('$scope','mainController',['$scope','commonService']function($scope,c_service){
-  c_service.getData('ertong',function(res){
-    console.dir(res)
-  })
-})
+app.controller('mainController',['$scope','commonService',function($scope,c_service){
+  // c_service.getData('ertong',function(res){
+  //   console.dir(res)
+  // })
+  $scope.selectIndex = 0;
+  $scope.linkTo = function(index){
+    $scope.selectIndex = index;
+  }}]
+)

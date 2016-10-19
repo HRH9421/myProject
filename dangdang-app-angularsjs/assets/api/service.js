0bb19b22d6c4c3eaf40890377c35f61c8cce7fcd
HRH9421 <hrh9421@foxmail.com>
2016-10-19 15:20:56 +0800

    初始化项目完整框架

---
+++ dangdang-app-angularsjs/assets/api/service.js
@@ -0,0 +1,21 @@
+//创建一个公用的数据管理仓库
+/**
+ * 根据分类信息取数据
+ * @type {Object}
+ */
+app.factory('commonService', ['$http', function($http) {
+    var service = {};
+    service.getData = function(type, callBack) {
+        $http({
+                url: '../../data/book_' + type + '.json',
+                method: 'get'
+            })
+            .then(function(res) {
+                console.dir(red)
+                callBack(res)
+            }, function(err) {
+                console.dir(err)
+            })
+    }
+    return service;
+}])
